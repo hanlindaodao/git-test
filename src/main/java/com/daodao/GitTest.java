a596240dbeddb573f37a0f7b3d05a53f39234bad
daodao <601760700@qq.com>
2022-04-10 10:42:59 +0800

    gitee push commit test

--- src/main/java/com/daodao/GitTest.java
+++ src/main/java/com/daodao/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master change 冲突");
         System.out.println("hot-fix has change test 冲突");
         System.out.println("change from browser and test the pull");
+        System.out.println("gitee push test");
     }
 }
